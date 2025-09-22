8aed4067da0ef06c55f57dbbb5ea0bf56c7f00cf
kobyakov <kobyakov@segmento.ru>
2025-09-22 23:07:02 +0300

    fix: correct calculation for report when only 1 day exists

--- src/CryptoWatcher/Extensions/PositionSnapshotExtensions.cs
+++ src/CryptoWatcher/Extensions/PositionSnapshotExtensions.cs
@@ -4,11 +4,27 @@ namespace CryptoWatcher.Extensions;
 
 public static class PositionSnapshotExtensions
 {
-    public static TSnapshot? GetNearestSnapshot<TSnapshot>(this IEnumerable<TSnapshot> snapshot, DateOnly day,
-        bool findPrevious) where TSnapshot : IPositionSnapshot
+    public static TSnapshot? GetNearestSnapshot<TSnapshot>(
+        this IReadOnlyCollection<TSnapshot> availableSnapshots, 
+        DateOnly day,
+        bool findPrevious) 
+        where TSnapshot : IPositionSnapshot
     {
+        if (availableSnapshots.Count == 0)
+        {
+            return default;
+        }
+
+        var daySnapshots = availableSnapshots.Where(s => s.Day == day).ToArray();
+        if (daySnapshots.Length > 1)
+        {
+            return findPrevious 
+                ? daySnapshots.MaxBy(s => s.Day)
+                : daySnapshots.MinBy(s => s.Day);
+        }
+
         return findPrevious
-            ? snapshot.Where(s => s.Day <= day).MaxBy(s => s.Day)
-            : snapshot.Where(s => s.Day >= day).MinBy(s => s.Day);
+            ? availableSnapshots.Where(s => s.Day <= day).MaxBy(s => s.Day)
+            : availableSnapshots.Where(s => s.Day >= day).MinBy(s => s.Day);
     }
 }
