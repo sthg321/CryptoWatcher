13389ffc2a9e3d098bfdab7df2e60f1dd897e07e
kobyakov <kobyakov@segmento.ru>
2026-01-26 22:20:16 +0300

    fix: add migrations

---
+++ src/CryptoWatcher.Infrastructure/Migrations/20260126191958_MakeLastTransactionNullabelMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWatcher.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeLastTransactionNullabelMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastTransactionHash",
+                table: "UniswapSynchronizationState",
+                type: "character(66)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 66,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character(66)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 66);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastTransactionHash",
+                table: "UniswapSynchronizationState",
+                type: "character(66)",
+                unicode: false,
+                fixedLength: true,
+                maxLength: 66,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character(66)",
+                oldUnicode: false,
+                oldFixedLength: true,
+                oldMaxLength: 66,
+                oldNullable: true);
+        }
+    }
+}
