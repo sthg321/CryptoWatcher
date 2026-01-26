d53fd4184a451b472af36df707c057a06d0b06ee
kobyakov <kobyakov@segmento.ru>
2026-01-26 13:23:11 +0300

    remove required from function name

--- src/modules/uniswap/CryptoWatcher.Modules.Uniswap.Application/Models/BlockchainTransaction.cs
+++ src/modules/uniswap/CryptoWatcher.Modules.Uniswap.Application/Models/BlockchainTransaction.cs
@@ -5,13 +5,13 @@ namespace CryptoWatcher.Modules.Uniswap.Application.Models;
 
 public class BlockchainTransaction
 {
-    public required string? FunctionName { get; init; } = null!;
+    public string? FunctionName { get; init; } = null!;
 
     public required TransactionHash Hash { get; init; } = null!;
 
     public required EvmAddress To { get; init; } = null!;
-    
+
     public required BigInteger BlockNumber { get; init; }
-    
+
     public required DateTime Timestamp { get; init; }
 }
