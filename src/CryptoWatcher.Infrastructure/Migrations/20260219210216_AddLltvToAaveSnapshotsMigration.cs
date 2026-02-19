2e1005ca5da86954053e8a23140d65ab18fd76ea
kobyakov <kobyakov@segmento.ru>
2026-02-20 00:05:38 +0300

    add migrations

---
+++ src/CryptoWatcher.Infrastructure/Migrations/20260219210216_AddLltvToAaveSnapshotsMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWatcher.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLltvToAaveSnapshotsMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "LiquidationLtv",
+                table: "AavePositionSnapshots",
+                type: "double precision",
+                nullable: true);
+
+
+            migrationBuilder.Sql("""
+                                 UPDATE "AavePositionSnapshots" 
+                                 SET "LiquidationLtv" = 0.80
+                                 WHERE "PositionId" IN 
+                                       (SELECT "Id" from "AavePositions" 
+                                                    WHERE "Token0_Symbol" = 'ETH' or "Token0_Symbol" = 'wstETH' or "Token0_Symbol" = 'WETH');
+                                 """);
+            
+            migrationBuilder.Sql("""
+                                 UPDATE "AavePositionSnapshots" 
+                                 SET "LiquidationLtv" = 0.73
+                                 WHERE "PositionId" IN 
+                                       (SELECT "Id" from "AavePositions" 
+                                                    WHERE "Token0_Symbol" = 'WBTC');
+                                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LiquidationLtv",
+                table: "AavePositionSnapshots");
+        }
+    }
+}
