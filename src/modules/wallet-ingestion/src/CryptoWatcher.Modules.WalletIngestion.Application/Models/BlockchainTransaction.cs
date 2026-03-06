2a91b8776bbf2f2933bd0416dc58bae599afb912
kobyakov <kobyakov@segmento.ru>
2026-03-06 19:21:10 +0300

    add entities and producer

---
+++ src/modules/wallet-ingestion/src/CryptoWatcher.Modules.WalletIngestion.Application/Models/BlockchainTransaction.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+using CryptoWatcher.ValueObjects;
+
+namespace CryptoWatcher.Modules.WalletIngestion.Application.Models;
+
+public class BlockchainTransaction
+{
+    public string? FunctionName { get; init; } = null!;
+
+    public required int ChainId { get; init; } 
+    
+    public required TransactionHash Hash { get; init; } = null!;
+
+    public required EvmAddress From { get; init; } = null!;
+    
+    public required EvmAddress To { get; init; } = null!;
+
+    public required BigInteger BlockNumber { get; init; }
+
+    public required DateTime Timestamp { get; init; }
+}
